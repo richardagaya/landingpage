bbfe6b9fd054ca0358e5a7bcf45dc6056f4e7de3
richardagaya <richardagaya278@gmail.com>
2025-01-02 21:45:37 +0300

    update about page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import Quiz from "./Quiz/page";
 import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 import Testimonials from "./components/Testimonial";
-import AboutUs from "./components/About";
 export default function Home() {
   return (
     <>
     <Navbar/>
     <HeroSection/>
     <Testimonials/>
-    <AboutUs/>
     <Footer/>
     </>
     
