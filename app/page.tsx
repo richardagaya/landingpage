f3747e54b3f3c6ff02e8fd70c06acd333dc9b0af
richardagaya <richardagaya278@gmail.com>
2025-01-10 16:34:41 +0300

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "./components/Hero";
 import Testimonials from "./components/Testimonial";
-import SellathonQuiz from "./components/SalesContentCreation";
 export default function Home() {
   return (
     <>
