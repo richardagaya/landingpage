6b5b9ab29e0eb880730a28944620486c10b517ca
Agaya <richardagaya278@gmail.com>
2025-01-15 14:49:47 +0300

    updated Skillstest.tsx

--- pages/Skillstest.tsx
+++ pages/Skillstest.tsx
@@ -69,7 +69,7 @@ const Quiz = () => {
             className="input bg-gray-700 text-white border border-gray-600 p-2 rounded w-full mb-4"
           />
           <button onClick={handleNameSubmit} className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Let's Go
+            Proceed
           </button>
         </div>
       </div>
