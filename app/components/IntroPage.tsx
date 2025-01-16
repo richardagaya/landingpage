91d273858c4c073a2bbe2d09f23f5ab4483eab2e
Agaya <richardagaya278@gmail.com>
2025-01-16 21:49:18 +0300

    video

--- app/components/IntroPage.tsx
+++ app/components/IntroPage.tsx
@@ -4,7 +4,7 @@ import VideoPlayer from '../components/VideoPlayer';
 const Home: React.FC = () => {
   return (
     <div className="p-8">
-      <VideoPlayer url="/Intro.MOV" />
+      <VideoPlayer url="/Intro.mp4" />
     </div>
   );
 };
